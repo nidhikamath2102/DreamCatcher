a39579a458b5e39b655f84ee143e0543ee9ddf73
moliva <moliva@vt.edu>
2024-02-17 12:01:27 -0500

    Prepare for Project 2B

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp") // BNRG Listing 12.8
+    id("androidx.navigation.safeargs.kotlin") // BNRNG 13.10
 }
 
 android {
@@ -15,6 +17,11 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunnerArguments["clearPackageData"] = "true"
+
+        ksp { // BNRG Chapter 12 Challenge
+            arg("room.schemaLocation", "$projectDir/schemas")
+        }
     }
 
     buildTypes {
@@ -39,6 +46,7 @@ android {
     @Suppress("UnstableApiUsage")
     testOptions {
         animationsDisabled = true
+        execution = "ANDROIDX_TEST_ORCHESTRATOR" // for P2B Testing
     }
 
 }
@@ -56,7 +64,18 @@ dependencies {
     implementation("androidx.test.espresso:espresso-contrib:3.5.1") // for Project 2 testing
     debugImplementation("androidx.fragment:fragment-testing:1.6.2") // for Project 2 testing
 
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0") // BNRG Listing 12.1
+    implementation("androidx.room:room-runtime:2.6.1") // BNRG Listing 12.8
+    implementation("androidx.room:room-ktx:2.6.1") // BNRG Listing 12.8
+    ksp("androidx.room:room-compiler:2.6.1") // BNRG Listing 12.8
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7") // BNRG Listing 13.1
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7") // BNRG Listing 13.1
+    androidTestImplementation("androidx.test:runner:1.5.2") // for P2B testing
+    androidTestUtil("androidx.test:orchestrator:1.4.2") // for P2B testing
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    androidTestImplementation("androidx.navigation:navigation-testing:2.7.7") // Project 2 testing
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+buildscript {
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7") // BNRG Listing 13.9
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.22-1.0.17") // BNRG Listing 12.9
+        classpath("com.android.tools.build:gradle:8.2.2")
+    }
 }
